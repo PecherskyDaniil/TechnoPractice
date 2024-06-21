e212f832656ea71b01bd723d449b6e4dd3e76138
PecherskyDaniil <cdtt38.pecherskyD@yandex.ru>
2024-06-21 05:00:14 +0000

    Setted bind type of volume for docker containers

--- src/loadalldockers.py
+++ src/loadalldockers.py
@@ -8,7 +8,7 @@ dcinside="version: '3'\nservices:\n"
 with open("docker-compose.yaml","w") as dcf:
     for filename in os.listdir(f"./rnxfiles/{getdate}"):
         if (filename[-3:]=="rnx"):
-           dcinside+=f"  {filename[:4].lower()}:\n    volumes:\n      - ./rnxfiles:/app/rnxfiles/\n    image: station:latest\n    command: python3 publisher.py {filename[:4].lower()}\n"
+           dcinside+=f"  {filename[:4].lower()}:\n    volumes:\n    - type: bind\n      source: ./rnxfiles\n      target: /app/rnxfiles/\n    image: station:latest\n    command: python3 publisher.py {filename[:4].lower()}\n"
     dcinside+=f"volumes:\n  rnxfiles:"
     dcf.write(dcinside)
 os.system(f"docker compose up")
