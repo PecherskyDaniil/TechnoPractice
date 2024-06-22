297d28efa2548f2a7c44289a40f14205b798bc53
PecherskyDaniil <cdtt38.pecherskyD@yandex.ru>
2024-06-22 07:17:12 +0000

    Added logs dir and bind mount for logs dir

--- src/loadalldockers.py
+++ src/loadalldockers.py
@@ -4,13 +4,12 @@ from datetime import date, timedelta
 def start_all_dockers(thisdate):
     getdate = thisdate
     os.system("docker build -t station .")
-    time.sleep(20)
     if (not(os.path.isfile("docker-compose.yaml"))):
         dcinside="version: '3'\nservices:\n"
         with open("docker-compose.yaml","w") as dcf:
             for filename in os.listdir(f"./rnxfiles/{getdate}"):
                 if (filename[-3:]=="rnx"):
-                    dcinside+=f"  {filename[:4].lower()}:\n    volumes:\n    - type: bind\n      source: ./rnxfiles\n      target: /app/rnxfiles/\n    image: station:latest\n    command: python3 publisher.py {filename[:4].lower()}\n"
-            dcinside+=f"volumes:\n  rnxfiles:"
+                    dcinside+=f"  {filename[:4].lower()}:\n    volumes:\n    - type: bind\n      source: ./rnxfiles\n      target: /app/rnxfiles/\n    - type: bind\n      source: ./logs\n      target: /app/logs/\n    image: station:latest\n    command: python3 publisher.py {filename[:4].lower()}\n"
+            dcinside+=f"volumes:\n  rnxfiles:\n  logs:"
             dcf.write(dcinside)
     os.system("docker compose up -d")
