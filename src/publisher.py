effaa0aa3871f89d0bdabcebb1e960da785a7c7d
PecherskyDaniil <cdtt38.pecherskyD@yandex.ru>
2024-06-22 11:01:19 +0000

    Replaced time.sleep with threading

--- src/publisher.py
+++ src/publisher.py
@@ -6,42 +6,59 @@ import paho.mqtt.client as mqtt_client
 import random
 from gnss_tec import rnx
 from sys import argv
+import threading
 
 getdate=date.today() - timedelta(days=90)
+
 def seconds_num(time):
     arrtime = time.split(":")
     return int(arrtime[0])*3600+int(arrtime[1])*60+int(arrtime[2])
 
+event=False
+
+def intervalPublish(tecit, curtec, client, fn, file):
+    global event
+    tec=curtec
+    result = fn+" "+tec.timestamp.strftime("%H:%M:%S")+'\n'
+    prevtec=tec
+    while tec.timestamp.strftime("%H:%M:%S") == prevtec.timestamp.strftime("%H:%M:%S"):
+        prevtec=tec
+        result+='{}: {} {}'.format(tec.satellite, tec.phase_tec, tec.p_range_tec,)+'\n'
+        try:
+            tec=tecit.__next__()
+        except:
+            client.disconnect()
+            file.close()
+            print("end")
+            event=True
+            return
+    client.publish("lab/leds/state",result)
+    print(result)
+    timer=threading.Timer(30.0, lambda: intervalPublish(tecit, tec, client, fn, file))
+    timer.start()
+
 def parseRNX(filename, clientid):
-    with open(filename) as obs_file:
-        reader = rnx(obs_file)
-        tecit = reader.next_tec()
-        tec=tecit.__next__()
-        #while seconds_num(strftime("%H:%M:%S", gmtime()))>seconds_num(tec.timestamp.strftime("%H:%M:%S")):
-        #    tec=tecit.__next__()
-        broker="broker.emqx.io"
-        client = mqtt_client.Client(
-            mqtt_client.CallbackAPIVersion.VERSION1, 
-            clientid
-        )
-        print("Connecting to broker",broker)
-        print(client.connect(broker))
-        print("Publishing")
-        client.loop_start() 
-        while tec.timestamp.strftime("%H:%M:%S")!='23:59:30':
-            result = filename+" "+tec.timestamp.strftime("%H:%M:%S")+'\n'
-            prevtec=tec
-            while tec.timestamp.strftime("%H:%M:%S") == prevtec.timestamp.strftime("%H:%M:%S"):
-                prevtec=tec
-                result+='{}: {} {}'.format(tec.satellite, tec.phase_tec, tec.p_range_tec,)+'\n'
-                tec = tecit.__next__()
-            client.publish("lab/leds/state",result)
-            print(result)
-            time.sleep(30)
-        client.disconnect()
-        client.loop_stop()
+    global event
+    obs_file=open(filename)
+    reader = rnx(obs_file)
+    tecit = reader.next_tec()
+    tec=tecit.__next__()
+    #while seconds_num("23:57:00")>seconds_num(tec.timestamp.strftime("%H:%M:%S")):
+    #    tec=tecit.__next__()
+    broker="broker.emqx.io"
+    client = mqtt_client.Client(
+        mqtt_client.CallbackAPIVersion.VERSION1, 
+        clientid
+    )
+    print("Connecting to broker",broker)
+    print(client.connect(broker))
+    print("Publishing") 
+    intervalPublish(tecit, tec, client, filename, obs_file)
+    while True:
+        if event:
+            break
 
-
+          
 script, stationname=argv
 while True:
     filename=""
@@ -50,6 +67,7 @@ while True:
             filename=f"/app/rnxfiles/{str(getdate)}/{filen}"
             break
     if filename!="":
+        event=False
         parseRNX(filename, stationname)
     else:
         time.sleep(30)
